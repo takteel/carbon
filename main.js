3cc00cbf5747b74454fb2aed31fd4b87a3020d55
takteel <xavierguillaume@hotmail.fr>
2013-12-10 13:04:33 +0100

    web-site boostrap

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
 "use strict";
+
+var settings = require('./settings');
+
+var http = require('http');
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+if (process.argv.length > 2) {
+	if (/^\d+$/.test(process.argv[2])) {
+		settings.port = parseInt(process.argv[2], 10);
+	}
+	else {
+		console.log('illegal port');
+	}
+}
+
+app.set('port', settings.port);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.compress());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+	app.use(express.errorHandler());
+}
+
+app.get('/', function(req, res) {
+	res.render('index', { title: 'index', routes: app.routes.get });
+});
+
+http.createServer(app).listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ settings.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var settings = {
+	port: 8080
+};
+
+module.exports = settings;
