6364b5da0ecdee5a2bf8c1b7e8df58df91bc1fc3
takteel <xavierguillaume@hotmail.fr>
2013-12-12 16:59:27 +0100

    design by contract continued

--- planet.js
+++ planet.js
@@ -8,16 +8,58 @@ function type(obj){
 var MakeContracts = function (cls) {
 	var util = require('util');
 
+	var newCls = undefined;
+	if (typeof cls === 'function') {
+		console.log('patching', cls);
+		newCls = function () {
+			console.log('-------------------------');
+			console.log('preconditions testing');
+			console.log('invariants testing');
+			var check, index, value, _i, _len;
+			for (var index = 0, _len = arguments.length; index < _len; ++index) {
+				console.log(index, arguments[index]);
+				value = arguments[index];
+				check = cls[index];
+				if (check) {
+					check(value);
+				}
+			}
+
+			var result = cls.apply(null, arguments);
+
+			console.log('invariants testing');
+			console.log('postconditions testing');
+			console.log('-------------------------');
+			
+			return result;
+		};
+	}
+
+	for (var i in cls) {
+		console.log('i:', i);
+		console.log('cls[i]:', cls[i]);
+	}
+
 	console.log('make contracts class', util.inspect(cls));
 	console.log('make contracts class.prototype', util.inspect(cls.prototype));
+	console.log('make contracts class.prototype.constructor', util.inspect(cls.prototype.constructor));
 
-	return cls;
+	return newCls;
+}
+
+function isNumber(param) {
+	if (typeof param !== 'number') {
+		throw RangeError('parameter is not a number');
+	}
 }
 
 var ContractObject = (function () {
 	function ContractObject(p0) {
+		console.log('ContractObject');
 	}
-	ContractObject.preconditions = [];
+	ContractObject.preconditions = [isNumber];
+	ContractObject.postconditions = [isNumber];
+	ContractObject.invariants = [isNumber];
 
 	ContractObject.prototype.test = function (p0, p1) {
 
@@ -28,6 +70,10 @@ var ContractObject = (function () {
 
 	return MakeContracts(ContractObject);
 })();
+
+var r = new ContractObject(42);
+var s = new ContractObject('42');
+var t = new ContractObject('test');
 
 var Planet = (function () {
 	function Planet(params) {
